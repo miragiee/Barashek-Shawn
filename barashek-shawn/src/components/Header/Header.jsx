a3377ede7ccab08e8e1d8ec5c70011112ce3e7e1
miragiee <miragiee564@gmail.com>
2026-06-09 13:14:54 +0300

    сверстал header

---
+++ barashek-shawn/src/components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import logo from '../assets/images/logo.svg';
+
+export default function Header(){
+    return(
+        <header>
+            <img src={logo} alt="Logo" />
+            <nav>
+                <ul>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
