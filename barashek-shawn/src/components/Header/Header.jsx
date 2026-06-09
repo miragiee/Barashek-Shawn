0c7ae63910e7d92eeed291667257eef622cb944f
miragiee <miragiee564@gmail.com>
2026-06-09 14:19:27 +0300

    Сделал Header
    !!КРОМЕ ССЫЛОК!!

--- barashek-shawn/src/components/Header/Header.jsx
+++ barashek-shawn/src/components/Header/Header.jsx
@@ -1,18 +1,20 @@
-import './Header.module.css'
+import styles from './Header.module.css'
 import logo from '../../assets/images/logo.svg';
+import shcart from '../../assets/images/Button 5.svg';
+import pfp from '../../assets/images/Button 6.svg';
 
 export default function Header(){
     return(
         <header>
-            <img src={logo} alt="Logo" />
+            <img src={logo} alt="Logo" className={styles.logo}/>
             <nav>
                 <ul>
-                    <li><a href=""></a></li>
-                    <li><a href=""></a></li>
-                    <li><a href=""></a></li>
-                    <li><a href=""></a></li>
-                    <li><a href=""></a></li>
-                    <li><a href=""></a></li>
+                    <li><a href="">Главная</a></li>
+                    <li><a href="">Забронировать</a></li>
+                    <li><a href="">Каталог</a></li>
+                    <li><a href="">Дегустации</a></li>
+                    <li><a href=""><img src={shcart} alt="Корзина" /></a></li>
+                    <li><a href=""><img src={pfp} alt="Профиль" /></a></li>
                 </ul>
             </nav>
         </header>
